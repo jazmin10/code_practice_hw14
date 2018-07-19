ea69ea740c270c08ea4ea5a15cde069def5ffe6f
jazmin10 <jazminestrada10@gmail.com>
2018-07-19 15:43:30 -0500

    setup express server

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+// ============== GLOBAL VARIABLES ==============
+	var express = require("express");
+
+	var router = express.Router();
+
+// ============== MAIN PROCESSES ==============
+
+	module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+// ============== GLOBAL VARIABLES ==============
+	var express = require("express");
+	var bodyParser = require("body-parser");
+	var exphbs = require("express-handlebars");
+	var methodOverride = require("method-override");
+	var routes = require("./controllers/burgers_controller.js");
+
+	var PORT = 3000;
+	var app = express();
+
+	// Middleware
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.text());
+	app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+	// Using handlebars as template
+	app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+	app.set("view engine", "handlebars");
+
+	// Method override
+	app.use(methodOverride("_method"));
+
+// ============== MAIN PROCESSES ==============
+	app.listen(PORT, function() {
+		console.log("listening on PORT:", PORT);
+	});
+
+	app.use("/", routes);
