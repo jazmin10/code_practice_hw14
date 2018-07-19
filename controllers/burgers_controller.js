6e7b287cb5d5c0caede4d899501b40108d6e6fd8
jazmin10 <jazminestrada10@gmail.com>
2018-07-19 15:54:46 -0500

    added express route and handlebars logic that displays burgers to the user

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,25 @@
 // ============== GLOBAL VARIABLES ==============
 	var express = require("express");
+	var burger = require("../models/burger.js");
 
 	var router = express.Router();
 
 // ============== MAIN PROCESSES ==============
 
+	// Add the following methods to the router object
+	
+	router.get("/", function(allReq, allResp) {
+		displayBurgers(allResp);
+	});
+
 	module.exports = router;
+
+// ============== FUNCTIONS ==============
+
+	// Display all of the burgers in the table
+	function displayBurgers(response) {
+		
+		burger.selectAll(function(results) {
+			response.render("index", {burgers: results});
+		});
+	}
